0320509123d99e28390094b180b703a79ea32acc
velpro <1326143109@qq.com>
2024-07-01 15:38:25 +0800

    296

---
+++ 00296-medium-permutation.ts
@@ -0,0 +1,38 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<Permutation<'A'>, ['A']>>,
+  Expect<
+    Equal<
+      Permutation<'A' | 'B' | 'C'>,
+      | ['A', 'B', 'C']
+      | ['A', 'C', 'B']
+      | ['B', 'A', 'C']
+      | ['B', 'C', 'A']
+      | ['C', 'A', 'B']
+      | ['C', 'B', 'A']
+    >
+  >,
+  Expect<
+    Equal<
+      Permutation<'B' | 'A' | 'C'>,
+      | ['A', 'B', 'C']
+      | ['A', 'C', 'B']
+      | ['B', 'A', 'C']
+      | ['B', 'C', 'A']
+      | ['C', 'A', 'B']
+      | ['C', 'B', 'A']
+    >
+  >,
+  Expect<Equal<Permutation<boolean>, [false, true] | [true, false]>>,
+  Expect<Equal<Permutation<never>, []>>
+]
+
+// ============= Your Code Here =============
+//https://github.com/type-challenges/type-challenges/issues/13383//
+type Permutation<T, K = T> = [T] extends [never]
+  ? []
+  : K extends K
+  ? [K, ...Permutation<Exclude<T, K>>]
+  : never
