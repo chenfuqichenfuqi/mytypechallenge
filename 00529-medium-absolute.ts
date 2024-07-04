fc96aa26091f61b79fcc4978876f0a69ba695922
velpro <1326143109@qq.com>
2024-07-04 09:36:31 +0800

    529

--- 00529-medium-absolute.ts
+++ 00529-medium-absolute.ts
@@ -11,9 +11,8 @@ type cases = [
   Expect<Equal<Absolute<'10'>, '10'>>,
   Expect<Equal<Absolute<'-5'>, '5'>>,
   Expect<Equal<Absolute<-1_000_000n>, '1000000'>>,
-  Expect<Equal<Absolute<9_999n>, '9999'>>,
+  Expect<Equal<Absolute<9_999n>, '9999'>>
 ]
 
-
 // ============= Your Code Here =============
-type Absolute<T extends number | string | bigint> = any
+type Absolute<T extends number | string | bigint> = `${T}` extends `-${infer U}` ? U : `${T}`
